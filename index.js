5e26f4d0c534e89cb98fa3957be386a48139bc93
angus <angusdippenaar@gmail.com>
2017-02-15 08:10:33 +0200

    Able to select days back/forwards 1, 2, 5 and 10. Write timeout 500ms

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ let day = 0;
 let writing = false;
 let contextMenu = returnContextMenu();
 
-String.prototype.lpad = function (length, char) {
-	const padding = char.repeat(length);
-
-	return padding.substr(0, padding.length - this.length) + this;
-};
-
-Number.prototype.lpad = function (length) {
-	return this.toString().lpad(length, '0');
-};
-
-Array.prototype.last = function () {
-	return this[this.length - 1];
-};
-
 app.on('ready', () => {
 	let image;
 	if (process.platform === 'darwin') {
@@ -38,16 +24,16 @@ app.on('ready', () => {
 
 	tray.on('click', typeDate);
 	
-	globalShortcut.register('CommandorControl+Shift+d', () => {
+	globalShortcut.register('CmdOrCtrl+Shift+d', () => {
 		console.log('Writing');
-		setTimeout(typeDate, 300);
+		setTimeout(typeDate, 500);
 	});
 });
 
 app.on('will-quit', globalShortcut.unregisterAll);
 
 function typeDate() {
-	if (!writing) {
+	if (writing === false) {
 		writing = true;
 		setTimeout(() => writing = false, 1000);
 
@@ -69,15 +55,15 @@ function getDateString(type) {
 
 	switch (type) {
 		case 'underscore':
-			return `${now.getFullYear()}_${(now.getMonth() + 1).lpad(2)}_${now.getDate()}`;
+			return `${now.getFullYear()}_${lpad(now.getMonth() + 1, 2)}_${now.getDate()}`;
 		case 'dayOfWeek':
 			return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][now.getDay()];
 		case 'timeLocale':
 			return now.toLocaleTimeString();
 		case 'timeWithoutSeconds':
-			return `${now.getHours().lpad(2)}:${now.getMinutes().lpad(2)}`;
+			return `${lpad(now.getHours(), 2)}:${lpad(now.getMinutes(), 2)}`;
 		case 'timeWithSeconds':
-			return `${now.getHours().lpad(2)}:${now.getMinutes().lpad(2)}:${now.getSeconds().lpad(2)}`;
+			return `${lpad(now.getHours(), 2)}:${lpad(now.getMinutes(), 2)}:${lpad(now.getSeconds(), 2)}`;
 		case 'timestamp':
 			return Math.floor(now.getTime() / 1000);
 		default:
@@ -90,17 +76,16 @@ function typeString(str) {
 
 	if (match && match[0]) {
 		str = str.split(match);
-		for (let chars of str) {
+		str.forEach((chars, index) => {
 			robot.typeString(chars);
 
-			if (str.last() !== chars) {
+			if (str.length - 1 !== index) {
 				robot.keyTap(match, 'shift')
 			}
-		}
+		});
 	} else {
 		robot.typeString(str);
 	}
-
 }
 
 function updateTimes() {
@@ -118,10 +103,46 @@ function updateTimes() {
 	tray.setContextMenu(Menu.buildFromTemplate(contextMenu));
 }
 
+function setActive(menuItem) {
+	contextMenu.forEach(item => {
+		if (item.date !== undefined) {
+			item.checked = (item.date === menuItem.date);
+		}
+	});
+}
+
+function updateDay(menuItem) {
+	day += parseInt(menuItem.label) * (menuItem.add ? 1 : -1);
+
+	updateTimes();
+}
+
+/**
+ * Takes a string or number and returns the string representation of that, left padded with the char of choice.
+ *   If the char is not given and the value is a number, the padding will be '0', and if the value is a string,
+ *   the padding will be spaces.
+ *
+ * @param {string|number}  value   String or number to left pad
+ * @param {number}         length  Total length of the final string
+ * @param {string}         [char]  Char to use as padding. If char is longer than 1 char, it will use only the beginning part for the padding
+ * @returns {string}
+ */
+function lpad(value, length, char) {
+	if (typeof value === 'number') {
+		value = value.toString();
+		char = char || '0';
+	}
+
+	if (typeof value === 'string') {
+		char = char || ' ';
+		return char.repeat(length).substr(0, length - value.length) + value;
+	}
+}
+
 function returnContextMenu() {
 	return [
 		{
-			label: 'write',
+			label: `Write					${process.platform === 'darwin' ? 'Command' : 'Ctrl'}+Shift+D`,
 			click: typeDate
 		},
 		{
@@ -130,60 +151,72 @@ function returnContextMenu() {
 		{
 			label: `System Date				${getDateString()}`,
 			date: 'systemDate',
-			type: 'radio'
+			type: 'radio',
+			checked: true,
+			click: setActive
 		},
 		{
 			label: `Underscore Date			${getDateString('underscore')}`,
 			date: 'underscore',
 			type: 'radio',
+			click: setActive
 		},
 		{
 			label: `Day of the week			${getDateString('dayOfWeek')}`,
 			date: 'dayOfWeek',
 			type: 'radio',
+			click: setActive
 		},
 		{
 			label: `System Time				${getDateString('timeLocale')}`,
 			date: 'timeLocale',
 			type: 'radio',
+			click: setActive
 		},
 		{
 			label: `Time without seconds	${getDateString('timeWithoutSeconds')}`,
 			date: 'timeWithoutSeconds',
 			type: 'radio',
+			click: setActive
 		},
 		{
 			label: `Time with seconds		${getDateString('timeWithSeconds')}`,
 			date: 'timeWithSeconds',
 			type: 'radio',
+			click: setActive
 		},
 		{
 			label: `Timestamp				${getDateString('timestamp')}`,
 			date: 'timestamp',
 			type: 'radio',
+			click: setActive
 		},
 		{
 			type: 'separator'
 		},
 		{
-			label: 'Subtract 1 day',
-			click: () => {
-				day--;
-				tray.setContextMenu(getContextMenu());
-			}
+			label: 'Subtract days',
+			submenu: [
+				{ label: '1', add: false, click: updateDay },
+				{ label: '2', add: false, click: updateDay },
+				{ label: '5', add: false, click: updateDay },
+				{ label: '10', add: false, click: updateDay }
+			]
 		},
 		{
-			label: 'Add 1 day',
-			click: () => {
-				day++;
-				tray.setContextMenu(getContextMenu());
-			}
+			label: 'Add days',
+			submenu: [
+				{ label: '1', add: true, click: updateDay },
+				{ label: '2', add: true, click: updateDay },
+				{ label: '5', add: true, click: updateDay },
+				{ label: '10', add: true, click: updateDay }
+			]
 		},
 		{
 			label: 'Go to today',
 			click: () => {
 				day = 0;
-				tray.setContextMenu(getContextMenu());
+				updateTimes();
 			}
 		},
 		{
@@ -191,7 +224,7 @@ function returnContextMenu() {
 		},
 		{
 			label: 'Exit',
-			click: () => app.quit()
+			click: app.quit
 		}
 	];
 }
